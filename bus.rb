398c59418e95fcf018855c53fbed1bfd0a14f03f
DeepCFoxXx <learncode23@gmail.com>
2018-03-23 15:29:55 +0000

    create bus passing

---
+++ bus.rb
@@ -0,0 +1,11 @@
+class Bus
+
+  attr_reader :route_number, :destination
+
+  def initialize(route_number, destination)
+    @route_number = route_number
+    @destination = destination
+    @passengers = []
+  end
+
+end
---
+++ specs/bus_spec.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bus.rb')
+require_relative('../person.rb')
+require_relative('../bus_stop.rb')
+
+class BusTest < MiniTest::Test
+
+  def setup
+    @bus = Bus.new(12, "Seafield")
+    @passenger1 = Person.new("Luke", 21)
+    @passenger2 = Person.new("Han", 33)
+    @passenger3 = Person.new("Liea", 19)
+  end
+
+  def test_can_create_bus
+    assert_equal(Bus, @bus.class)
+  end
+
+end
